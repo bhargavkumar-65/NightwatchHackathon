462b35923f9d0af790aaa3e3079073c9b49d05df
Bhargav Murarisetty <Bhargav.murarisetty@amdocs.com>
2020-02-26 00:01:48 +0530

    Commit 2

--- vrt-tests/visualvalidation.js
+++ vrt-tests/visualvalidation.js
@@ -1,21 +1,22 @@
 //.RfTNa.sc-eNQAEJ a[data-di-id="di-id-8148ed7f-58b44f6b"]
 
 module.exports = {
-    'Visual Regression Test1: Validate Telus LOGO'(browser) {
+    'VRT1-Validate Telus LOGO'(browser) {
         const logoimage = '.khYKIO.sc-jKJlTe.sc-epnACN a[data-di-id="di-id-8148ed7f-58b44f6b"] img[alt="TELUS Logo"]';
         browser
+            .maximizeWindow()
             .url('https://telus.com/')
-            .waitForElementVisible(logoimage,20000)
-            .assert.screenshotIdenticalToBaseline(logoimage, 'logo');
+            .waitForElementVisible(logoimage,40000)
+            .assert.screenshotIdenticalToBaseline(logoimage, 'logo')
     },
-    'Visual Regression Test2: Validate Graffee Image'(browser) {
+    'VRT2-Validate Graffee Image'(browser) {
         const GiraffeeImg = 'img[alt="Giraffe Whole Home Offer"]';
         browser
             .url('https://telus.com/')
-            .waitForElementVisible(GiraffeeImg,20000)
-            .assert.screenshotIdenticalToBaseline(GiraffeeImg, 'Giraffee');
+            .waitForElementVisible(GiraffeeImg,40000)
+            .assert.screenshotIdenticalToBaseline(GiraffeeImg, 'Giraffee')
     },
-    'Visual Regression Test3: Validate GET STARTED BUTTON COLOUR'(browser) {
+    'VRT3-Validate GET STARTED BUTTON COLOUR'(browser) {
         const GetStartedBtn = '.iyApTw.Button__StyledButton-sa7xo3-0.sc-kGXeez';
         browser
             .url('https://telus.com/')
